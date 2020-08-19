543bf9382a16734cc77e0af41cce777bd7d357d1
MuhammadEssam96 <muhammad.essam96@gmail.com>
2020-08-19 14:41:01 +0200

    Add SafeArea around the navbar

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -26,147 +26,149 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: 64.0,
-          decoration: BoxDecoration(
-            color: AppColors.white,
-            borderRadius: BorderRadius.all(Radius.circular(50)),
-            shape: BoxShape.rectangle
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Material(
-                  type: MaterialType.transparency,
-                  child: InkWell(
-                    customBorder: CircleBorder(),
-                    splashColor: currentBottomNavBarItemIndex == 0 ? AppColors.purpleLight : AppColors.tealLight,
-                    onTap: (){
-                      setState(() {
-                        currentBottomNavBarItemIndex = 0;
-                      });
-                    },
-                    child: Container(
-                      width: 56,
-                      height: 56,
-                      decoration: BoxDecoration(
-                        color: currentBottomNavBarItemIndex == 0 ? AppColors.tealLight : AppColors.purpleLight,
-                        borderRadius: BorderRadius.all(Radius.circular(50))
-                      ),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.home,
-                            semanticLabel: "Home",
-                            color: currentBottomNavBarItemIndex == 0 ? AppColors.teal : AppColors.purple
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                Material(
-                  type: MaterialType.transparency,
-                  child: InkWell(
-                    customBorder: CircleBorder(),
-                    splashColor: currentBottomNavBarItemIndex == 1 ? AppColors.purpleLight : AppColors.tealLight,
-                    onTap: (){
-                      setState(() {
-                        currentBottomNavBarItemIndex = 1;
-                      });
-                    },
-                    child: Container(
-                      width: 56,
-                      height: 56,
-                      decoration: BoxDecoration(
-                        color: currentBottomNavBarItemIndex == 1 ? AppColors.tealLight : AppColors.purpleLight,
-                        borderRadius: BorderRadius.all(Radius.circular(50))
-                      ),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.shopping_cart,
-                            semanticLabel: "Cart",
-                            color: currentBottomNavBarItemIndex == 1 ? AppColors.teal : AppColors.purple,
-                          ),
-                        ],
+      bottomNavigationBar: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 64.0,
+            decoration: BoxDecoration(
+              color: AppColors.white,
+              borderRadius: BorderRadius.all(Radius.circular(50)),
+              shape: BoxShape.rectangle
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Material(
+                    type: MaterialType.transparency,
+                    child: InkWell(
+                      customBorder: CircleBorder(),
+                      splashColor: currentBottomNavBarItemIndex == 0 ? AppColors.purpleLight : AppColors.tealLight,
+                      onTap: (){
+                        setState(() {
+                          currentBottomNavBarItemIndex = 0;
+                        });
+                      },
+                      child: Container(
+                        width: 56,
+                        height: 56,
+                        decoration: BoxDecoration(
+                          color: currentBottomNavBarItemIndex == 0 ? AppColors.tealLight : AppColors.purpleLight,
+                          borderRadius: BorderRadius.all(Radius.circular(50))
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.home,
+                              semanticLabel: "Home",
+                              color: currentBottomNavBarItemIndex == 0 ? AppColors.teal : AppColors.purple
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Material(
-                  type: MaterialType.transparency,
-                  child: InkWell(
-                    customBorder: CircleBorder(),
-                    splashColor: currentBottomNavBarItemIndex == 2 ? AppColors.purpleLight : AppColors.tealLight,
-                    onTap: (){
-                      setState(() {
-                        currentBottomNavBarItemIndex = 2;
-                      });
-                    },
-                    child: Container(
-                      width: 56,
-                      height: 56,
-                      decoration: BoxDecoration(
-                        color: currentBottomNavBarItemIndex == 2 ? AppColors.tealLight : AppColors.purpleLight,
-                        borderRadius: BorderRadius.all(Radius.circular(50))
-                      ),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.favorite,
-                            semanticLabel: "Wishlist",
-                            color: currentBottomNavBarItemIndex == 2 ? AppColors.teal : AppColors.purple,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                Material(
-                  type: MaterialType.transparency,
-                  child: InkWell(
-                    customBorder: CircleBorder(),
-                    splashColor: currentBottomNavBarItemIndex == 3 ? AppColors.purpleLight : AppColors.tealLight,
-                    onTap: (){
-                      setState(() {
-                        currentBottomNavBarItemIndex = 3;
-                      });
-                    },
-                    child: Container(
-                      width: 56,
-                      height: 56,
-                      decoration: BoxDecoration(
-                        color: currentBottomNavBarItemIndex == 3 ? AppColors.tealLight : AppColors.purpleLight,
-                        borderRadius: BorderRadius.all(Radius.circular(50))
-                      ),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.person,
-                            semanticLabel: "Profile",
-                            color: currentBottomNavBarItemIndex == 3 ? AppColors.teal : AppColors.purple,
-                          ),
-                        ],
+                  Material(
+                    type: MaterialType.transparency,
+                    child: InkWell(
+                      customBorder: CircleBorder(),
+                      splashColor: currentBottomNavBarItemIndex == 1 ? AppColors.purpleLight : AppColors.tealLight,
+                      onTap: (){
+                        setState(() {
+                          currentBottomNavBarItemIndex = 1;
+                        });
+                      },
+                      child: Container(
+                        width: 56,
+                        height: 56,
+                        decoration: BoxDecoration(
+                          color: currentBottomNavBarItemIndex == 1 ? AppColors.tealLight : AppColors.purpleLight,
+                          borderRadius: BorderRadius.all(Radius.circular(50))
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.shopping_cart,
+                              semanticLabel: "Cart",
+                              color: currentBottomNavBarItemIndex == 1 ? AppColors.teal : AppColors.purple,
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ]
+                  Material(
+                    type: MaterialType.transparency,
+                    child: InkWell(
+                      customBorder: CircleBorder(),
+                      splashColor: currentBottomNavBarItemIndex == 2 ? AppColors.purpleLight : AppColors.tealLight,
+                      onTap: (){
+                        setState(() {
+                          currentBottomNavBarItemIndex = 2;
+                        });
+                      },
+                      child: Container(
+                        width: 56,
+                        height: 56,
+                        decoration: BoxDecoration(
+                          color: currentBottomNavBarItemIndex == 2 ? AppColors.tealLight : AppColors.purpleLight,
+                          borderRadius: BorderRadius.all(Radius.circular(50))
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.favorite,
+                              semanticLabel: "Wishlist",
+                              color: currentBottomNavBarItemIndex == 2 ? AppColors.teal : AppColors.purple,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Material(
+                    type: MaterialType.transparency,
+                    child: InkWell(
+                      customBorder: CircleBorder(),
+                      splashColor: currentBottomNavBarItemIndex == 3 ? AppColors.purpleLight : AppColors.tealLight,
+                      onTap: (){
+                        setState(() {
+                          currentBottomNavBarItemIndex = 3;
+                        });
+                      },
+                      child: Container(
+                        width: 56,
+                        height: 56,
+                        decoration: BoxDecoration(
+                          color: currentBottomNavBarItemIndex == 3 ? AppColors.tealLight : AppColors.purpleLight,
+                          borderRadius: BorderRadius.all(Radius.circular(50))
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.person,
+                              semanticLabel: "Profile",
+                              color: currentBottomNavBarItemIndex == 3 ? AppColors.teal : AppColors.purple,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ]
+              ),
             ),
           ),
         ),
