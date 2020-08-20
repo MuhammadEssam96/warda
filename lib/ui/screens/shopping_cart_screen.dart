5d45be2ad86e46b91f687d6c5612d3a7166918ab
Amany Gharib <amanygh12@gmail.com>
2020-08-19 18:27:22 -0700

    add list view

--- lib/ui/screens/shopping_cart_screen.dart
+++ lib/ui/screens/shopping_cart_screen.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:warda/ui/models/cart.dart';
+import 'package:warda/ui/values/colors.dart';
 import 'package:warda/widgets/shopping_card_widget.dart';
-import '../values/colors.dart';
+
 
 
 class ShoppingCartScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     var mediaQ=MediaQuery.of(context).size.width;
-    return ShoppingCartWidget(mediaQ: mediaQ);
+    return Column(children: [
+      Expanded(
+        flex: 1,
+              child: Text('Shopping cart',style:TextStyle(
+          color: AppColors.grayDark,
+          fontSize: 20,
+        ),),
+      ),
+      // SizedBox(height: 2,),
+      Expanded(
+        flex: 10,
+              child: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (context,index){
+          return ShoppingCartWidget(mediaQ: mediaQ);
+        },),
+      ),
+    ],
+    );
+    
   }
 }
 
