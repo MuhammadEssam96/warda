49be803e55834cca3f9b81f3e21b57cb643033cd
Amany Gharib <amanygh12@gmail.com>
2020-08-19 18:17:44 -0700

    add gesture detector

--- lib/widgets/card_button.dart
+++ lib/widgets/card_button.dart
@@ -3,17 +3,22 @@ import 'package:flutter/material.dart';
 import '../ui/values/colors.dart';
 
 class CardButton extends StatelessWidget {
-  CardButton({this.iconButton});
+  CardButton({this.iconButton, this.buttonFunction});
   final IconData iconButton;
+  final Function buttonFunction;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width:30 ,
-      height: 30,
-      decoration: BoxDecoration(color: Colors.teal[100],
-      borderRadius: BorderRadius.circular(25)
-       ),
-       child: Icon(iconButton,color: AppColors.grayDark,size: 15,),
-      );
+    return GestureDetector(
+      onTap: buttonFunction,
+          child: Container(
+        width:30 ,
+        height: 30,
+        decoration: BoxDecoration(color: Colors.teal[100],
+        borderRadius: BorderRadius.circular(25)
+         ),
+         child: Icon(iconButton,color: AppColors.grayDark,size: 15,),
+         
+        ),
+    );
   }
 }
